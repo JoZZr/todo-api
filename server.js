04ddc0a3fc000747f2e7cf5af679d3792462b32f
Johannes Reich <reichjoh@gmail.com>
2016-04-23 16:42:57 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
-const PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: "Finishing node.js course",
-	completed: false
-}, {
-	id: 2,
-	description: "Got to market",
-	completed: false
-}, {
-	id: 3,
-	description: "Party",
-	completed: true
-}];
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
 	res.send("Todo API root");
 });
 
+//GET /todos
 app.get("/todos", function (req, res) {
 	res.json(todos);
 });
 
+//GET /todos/:id
 app.get("/todos/:id", function (req, res) {
 	var todoId = parseInt(req.params.id);
 	var matchedToDo;
+
 	for (var i = 0; i < todos.length; i++) {
 		if (todoId === todos[i].id) {
 			matchedToDo = todos[i];
@@ -38,6 +34,19 @@ app.get("/todos/:id", function (req, res) {
 		res.status(404).send();
 	}
 });
+
+//POST /todos
+app.post("/todos", function (req, res) {
+	var body = req.body;
+
+	console.log("description: " + body.description);
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log("Express listening on port " + PORT + "!"); 
