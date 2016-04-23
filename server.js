9208a93f79bf632f6d3050e10c16571d5bc0c150
Johannes Reich <reichjoh@gmail.com>
2016-04-23 18:19:17 +0200

    Bugfix

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.delete("/todos/:id", function (req, res) {
 		todos = _.without(todos, matchedToDo);
 		res.json(matchedToDo);
 	} else {
-		res.status(404).json({"error: no todo found with that id"});
+		res.status(404).json({"error": "no todo found with that id"});
 	}
 });
 
