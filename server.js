c01e7f12594c808718d9778402336e79369e4cf2
Johannes Reich <reichjoh@gmail.com>
2016-04-24 19:50:10 +0200

    Removed old code

--- server.js
+++ server.js
@@ -37,25 +37,6 @@ app.get("/todos", function(req, res) {
 		res.status(500).send();
 	});
 
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
-	// 	filteredTodos = _.where(todos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
-	// 	filteredTodos = _.where(todos, {
-	// 		completed: false
-	// 	});
-	// }
-
-	// if (queryParams.hasOwnProperty("q") && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) !== -1;
-	// 	})
-	// }
-
-	// res.json(filteredTodos);
 });
 
 //GET /todos/:id
@@ -72,15 +53,6 @@ app.get("/todos/:id", function(req, res) {
 		return res.status(500).send();
 	});
 
-	// var matchedToDo = _.findWhere(todos, {
-	// 	id: todoId
-	// });
-
-	// if (matchedToDo) {
-	// 	res.json(matchedToDo);
-	// } else {
-	// 	res.status(404).send();
-	// }
 });
 
 //POST /todos
@@ -94,18 +66,6 @@ app.post("/todos", function(req, res) {
 		return res.status(400).json(e);
 	});
 
-	//if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).json(e);
-	//}
-
-	//	respond with 200 and todo
-	//	res.status(400).json(e)
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-	// todos.push(body);
-
-	// res.json(body);
 });
 
 //DELETE /todos:id
