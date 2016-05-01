cab0eb8c9c344acc9fc8d8d5e696ff77da2a6077
Johannes Reich <reichjoh@gmail.com>
2016-05-01 12:56:03 +0200

    Added user authentication

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require("bcrypt");
 var _ = require("underscore");
+var cryptojs = require("crypto-js");
+var jwt = require("jsonwebtoken");
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define("user", {
@@ -40,12 +42,6 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		},
-		instanceMethods: {
-			toPublicJSON: function() {
-				var json = this.toJSON();
-				return _.pick(json, "id", "email", "createdAt", "updatedAt");
-			}
-		},
 		classMethods: {
 			authenticate: function(body) {
 				return new Promise(function(resolve, reject) {
@@ -68,6 +64,33 @@ module.exports = function(sequelize, DataTypes) {
 						return reject();
 					});
 				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, "id", "email", "createdAt", "updatedAt");
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({
+						id: this.get("id"),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, "abc123!@#!").toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, "qwertz098");
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
